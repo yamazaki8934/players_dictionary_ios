c2ea664c1c597eba58ef6eaf4201cede18751665
yamazaki8934 <jdmvgaj@gmail.com>
2019-04-27 16:19:21 +0900

    Initial Commit

---
+++ players_dictionary_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  players_dictionary_ios
+//
+//  Created by 山崎浩毅 on 2019/04/27.
+//  Copyright © 2019年 山崎浩毅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
